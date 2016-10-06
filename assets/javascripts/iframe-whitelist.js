271c37b50c813c2da82997ec1f4d87b8e7836a8e
Mike Taber <miketaber@users.noreply.github.com>
2016-10-06 12:38:38 -0400

    Update iframe-whitelist.js

--- assets/javascripts/iframe-whitelist.js
+++ assets/javascripts/iframe-whitelist.js
@@ -1,10 +1,7 @@
 (function() {
   if (Discourse.dialect_deprecated) { return; }
 
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.freesound\.org\/embed\/sound\/iframe\/.+/i);
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/w\.soundcloud\.com\/player\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.youtube\.com\/embed\/.+/i);
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/discordapp\.com\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/chatroll\.com\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.chatroll\.com\/embed\/.+/i);
 })();
